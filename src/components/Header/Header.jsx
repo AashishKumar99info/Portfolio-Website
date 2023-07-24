76cf0583e48bb5d8534ca82ebdacf399ce5c9d26
AashishKumar99info <125687064+AashishKumar99info@users.noreply.github.com>
2023-07-24 06:54:24 +0000

    In header menu and right menu created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -22,6 +22,35 @@ const Header = () => {
             </div>
           </div>
           {/* ==================Logo End=================== */}
+          {/* =================menu start================== */}
+          <div className='menu'>
+            <ul className='flex items-center gap-10'>
+              <li>
+                <a className='text-smallTextColor font-[600]' href="#about">About</a>
+              </li>
+              <li>
+                <a className='text-smallTextColor font-[600]' href="#services">Services</a>
+              </li>
+              <li>
+                <a className='text-smallTextColor font-[600]' href="#portfolio">Portfolio</a>
+              </li>
+              <li>
+                <a className='text-smallTextColor font-[600]' href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+          {/* =================menu end================== */}
+
+          {/* =================menu right================ */}
+          <div className="flex itmes-center gap-4">
+            <button className='flex items-center gap-2 text-smallTextColor font-[600] border border-solid border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[500] ease-in duration-300' >
+              <i class="ri-send-plane-line"></i>Let's Talk
+            </button>
+            <span className="text-2xl text-smallTextColor md:hidden cursor-pointer">
+              <i class="ri-menu-line"></i>
+            </span>
+          </div>
+          {/* =================menu right end================ */}
         </div>
       </div>
     </header>
