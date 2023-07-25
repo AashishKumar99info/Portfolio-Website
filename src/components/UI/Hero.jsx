3ad28209fd7e4342a001926830b18fbb985082c6
AashishKumar99info <125687064+AashishKumar99info@users.noreply.github.com>
2023-07-25 07:11:13 +0000

    experience and projects details added

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -104,12 +104,56 @@ const Hero = () => {
           </div>
           {/* ==========hero left end============= */}
           {/* ==========hero img start============= */}
-            <div className="basis-1/3 mt-10 sm:mt-0">
-              <figure className=' flex items-center justify-center'>
-                <img src={heroImg} alt="" />
-              </figure>
-            </div>
+          <div className="basis-1/3 mt-10 sm:mt-0">
+            <figure className=' flex items-center justify-center'>
+              <img src={heroImg} alt="" />
+            </figure>
+          </div>
           {/* ==========hero img end============= */}
+          {/* ==========hero content right============= */}
+          <div className='md:basis-1/5 flex justify-between text-center mt-10 flex-wrap gap-3 md:mt-0 md:flex-col md:justify-end md:text-end'>
+            {/* <div className="mb-10">
+              <h2 className='text-headingColor font-[700] text-[32px]' >
+                <CountUp start={0} end={6} duration={2} suffix='+' />
+              </h2>
+              <h4 className='text-headingColor font-[600] text-[18px]' >
+                Years of experience
+              </h4>
+            </div> */}
+            <div className="mb-10">
+              <h2 className='text-headingColor font-[700] text-[32px]' >
+                <CountUp start={0} end={2} duration={2} suffix='+' />
+              </h2>
+              <h4 className='text-headingColor font-[600] text-[18px]' >
+                Years of Experience
+              </h4>
+            </div>
+            <div className="mb-10">
+              <h2 className='text-headingColor font-[700] text-[32px]' >
+                <CountUp start={0} end={100} duration={2} suffix='%' />
+              </h2>
+              <h4 className='text-headingColor font-[600] text-[18px]' >
+                Success Rate
+              </h4>
+            </div>
+            <div className="mb-10">
+              <h2 className='text-headingColor font-[700] text-[32px]' >
+                <CountUp start={0} end={102} duration={2} suffix='+' />
+              </h2>
+              <h4 className='text-headingColor font-[600] text-[18px]' >
+                Happy Clients
+              </h4>
+            </div>
+            <div className="mb-10">
+              <h2 className='text-headingColor font-[700] text-[32px]' >
+                <CountUp start={0} end={112} duration={2} suffix='+' />
+              </h2>
+              <h4 className='text-headingColor font-[600] text-[18px]' >
+                Projects Completed
+              </h4>
+            </div>
+          </div>
+          {/* ==========hero content right end============= */}
         </div>
       </div>
     </section>
